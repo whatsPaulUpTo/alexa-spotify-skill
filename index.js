afe5651352cbfa669fa1d1de176355ded3920abb
Paul Shearer <whatsPaulUpTo@gmail.com>
2017-06-20 19:57:31 -0500

    working version - check intents
    make sure intents pass testing when commands have possibility of triggering non-intented "intents"

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-'use strict';
 module.change_code = 1;
 var Alexa = require('alexa-app');
 var app = new Alexa.app('music-mastermind');
 var rp = require('request-promise');
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
+var client_id = 'your-client-id-here'; // Your client id
+var client_secret = 'your-client-secret-here'; // Your secret
 var token;
 var options;
 var songName;
 var songObj;
 var trackID;
 var songAnalysis;
-var alexaResponse;
+var reprompt = 	'What song do you want to know the key and tempo for?';
+var alexaResponse = 'Sorry, I was unable to retrieve the song data. Please try again.';
+var saySong;
+var sayArtist;
+var sayTempo;
+var sayKey;
+
+var exitFunction = function(request, response) {
+  var stopOutput = 'Thanks for using Music Mastermind! Goodbye';
+  response.say(stopOutput).shouldEndSession(true);
+};
 
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -28,8 +37,9 @@ var authOptions = {
 
 // *** LAUNCH APP ***
 app.launch(function(req, res) {
-  var prompt = 'Tell me the name of the song you want to know the key and tempo for.';
-  res.say(prompt).reprompt(prompt).shouldEndSession(false);
+	var prompt = 	'Welcome to Music Mastermind. What song do you want to know the key and tempo for? For example, you can say, "what is the key and tempo of I Will Survive?"'
+	res.say(prompt).reprompt(prompt).shouldEndSession(false);
+	return;
 });
 
 // *** MAIN INTENT ***
@@ -37,15 +47,14 @@ app.intent('getKeyAndTempo', {
 		'slots': {
 			'SONGNAME': 'SONGNAMES'
 		},
-		'utterances': ['{|about|for} {-|SONGNAME}']
+		'utterances': ['{what is|tell me} {|the key and tempo|the tempo and key|the key|the tempo} {about|for|of} {-|SONGNAME}']
 	}, 
 	function(req, res) {
 		//get the slot
 		songName = req.slot('SONGNAME');
-		var reprompt = 'Tell me the name of the song you want to know the key and tempo for.';
 
-		if (songName == '') {
-			var prompt = 'I didn\'t hear a song name. Tell me a song name.';
+		if (!songName) {
+			var prompt = 'I didn\'t understand. Please tell me what song you want to know the key and tempo for. For example, you can say, "what is the key and tempo of I Will Survive?"';
 			res.say(prompt).reprompt(reprompt).shouldEndSession(false);
 			return true;
 		} else {
@@ -59,16 +68,47 @@ app.intent('getKeyAndTempo', {
       		).then(getSongObj
       		).then(getAnalysis
       		).then(formatResponse
+      		).then(buildResponse
+      		).then(function() {
+      			//alexa response
+      			console.log(alexaResponse);
+      			res.say(alexaResponse).send();
+      		}
       		).catch(function(err) {
 				console.log(err.statusCode);
-				var prompt = 'I couldn\'t find information for ' + songName;
+				songName = songName.replace(/\+/g, ' ');
+				var prompt = 'Sorry. I couldn\'t find information for ' + songName + '. Please ask me about a different song.';
 				res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
-			})
-			//Alexa says answer
-			res.say(alexaResponse).send();
-		};
+			});
+
+			//alexa response
+			console.log(alexaResponse);
+			res.say(alexaResponse).shouldEndSession(true).send();
+		}
 	}
 );
+
+app.intent('AMAZON.HelpIntent', {
+
+	"slots": {},
+	"utterances": []
+}, function(request, response) {
+	var helpOutput = 'To find out the key and tempo of a song, request it by it\'s title.  For example, to get information about "I Will Survive", say,  What is the key and tempo of "I Will Survive"?  You can also say stop or cancel to exit. What song would you like me to tell you about?';
+	response.say(helpOutput).shouldEndSession(false);
+	return;
+});
+
+app.intent('AMAZON.StopIntent', {
+
+	"slots": {},
+	"utterances": []
+}, exitFunction);
+
+app.intent('AMAZON.CancelIntent', {
+
+	"slots": {},
+	"utterances": []
+}, exitFunction);
 
 // *** SPOTIFY API FUNCTIONS ***
 
@@ -82,13 +122,14 @@ function getSongObj() {
     };
 
     return rp(options);
-};
+}
 
 function getAnalysis(song) {
 	songObj = song;
 	trackID = songObj.tracks.items[0].id;
+	console.log(trackID);
 	options = {
-		url: 'https://api.spotify.com/v1/audio-analysis/' + trackID,
+		url: 'https://api.spotify.com/v1/audio-features/' + trackID,
 		headers: {
 			'Authorization': 'Bearer ' + token
 		},
@@ -96,44 +137,44 @@ function getAnalysis(song) {
 	};
 
 	return rp(options);
-};
+}
 
 function formatResponse(analysis) {
 	songAnalysis = analysis;
-	var saySong = songObj.tracks.items[0].name;
-	var sayArtist = formatArtist(songObj.tracks.items[0].artists);
-	var sayTempo = Math.round(songAnalysis.track.tempo);
-	var sayKey = formatKey(songAnalysis);
+	saySong = (songObj.tracks.items[0].name).replace(/&/g, 'and');
+	sayArtist = formatArtist(songObj.tracks.items[0].artists);
+	sayTempo = Math.round(songAnalysis.tempo);
+	sayKey = formatKey(songAnalysis);
 
-	alexaResponse = 'Here\'s what I got for ' 
-					+ saySong + ', by ' + sayArtist 
-					+ '. It is in the key of ' + sayKey 
-					+ ', and it has a tempo of ' + sayTempo + ' beats per minute.';
-};
+}
 
-// var formatConfidence = function(confidence) {
-// 	var confidenceStr;
-// 	// put logic to turn confidence metrics into something user freindly. break down into not sure? pretty sure?
-// }
+function buildResponse() {
+	alexaResponse = 'Here\'s what I got for ' + saySong + ', by ' + sayArtist + '. It is in the key of ' + sayKey + ', and it has a tempo of ' + sayTempo + ' beats per minute.';
+
+	console.log(alexaResponse);
+
+	return alexaResponse;
+}
 
 function formatArtist(artists) {
-	var artistStr;
+	var artistStr = '';
 	for (var i = 0; i < artists.length; i++) {
 		if (artists[i-1] && !artists[i+1]) {
 			artistStr += 'and ';
-		};
+		}
 		artistStr += artists[i].name;
 		if (artists[i+1]) {
 			artistStr += ', ';
-		};
-	};
-	return artistStr;
-};
+		}
+	}
+
+	return artistStr.replace(/&/g, 'and');
+}
 
 function formatKey(analysisObj) {
-	var keyString;
+	var keyString = '';
 
-	switch (analysisObj.track.key) {
+	switch (analysisObj.key) {
 		case 0:
 			keyString += 'C ';
 			break;
@@ -153,7 +194,7 @@ function formatKey(analysisObj) {
 			keyString += 'F ';
 			break;
 		case 6:
-			keyString += 'F sharp';
+			keyString += 'F sharp ';
 			break;
 		case 7:
 			keyString += 'G ';
@@ -171,10 +212,10 @@ function formatKey(analysisObj) {
 			keyString += 'B ';
 			break;
 		default:
-			console.log('There shouldn\'t be a default case for the key');
-	};
+			keyString += 'undefined key ';
+	}
 
-	switch (analysisObj.track.mode) {
+	switch (analysisObj.mode) {
 		case 0:
 			keyString += 'minor';
 			break;
@@ -182,11 +223,11 @@ function formatKey(analysisObj) {
 			keyString += 'major';
 			break;
 		default:
-			console.log('There shouldn\'t be a default case for the mode');
-	};
+			keyString += 'with undefined mode ';
+	}
 
 	return keyString;
-};
+}
 
 //hack to support custom utterances in utterance expansion string
 console.log(app.utterances().replace(/\{\-\|/g, '{'));
